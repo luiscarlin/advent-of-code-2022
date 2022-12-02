2b37cfddc65572d95bd4caa70b7e9dd0a0ea5d75
Luis Carlin <luiscarlin@gmail.com>
2022-12-02 00:58:32 -0500

    cleaned it up even more. Feel better about this

--- 02/puzzle.ts
+++ 02/puzzle.ts
@@ -1,134 +1,52 @@
 import { _format } from "https://deno.land/std@0.166.0/path/_util.ts";
 import { sum } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
-const calculatePoints = (other: string, me: string) => {
-  let points = 0;
-  if (other === "A") {
-    if (me === "X") {
-      points = 3;
-    }
-    if (me === "Y") {
-      points = 6;
-    }
-    if (me === "Z") {
-      points = 0;
-    }
-  }
-  if (other === "B") {
-    if (me === "X") {
-      points = 0;
-    }
-    if (me === "Y") {
-      points = 3;
-    }
-    if (me === "Z") {
-      points = 6;
-    }
-  }
+// A rock
+// B paper
+// C scissors
 
-  if (other === "C") {
-    if (me === "X") {
-      points = 6;
-    }
-    if (me === "Y") {
-      points = 0;
-    }
-    if (me === "Z") {
-      points = 3;
-    }
-  }
+// X rock
+// Y paper
+// Z scissors
 
-  points += me === "X" ? 1 : me === "Y" ? 2 : 3;
+// X I lose
+// Y draw
+// Z I win
 
-  return points;
+const points: { [key: string]: number } = {
+  "A X": 3 + 1,
+  "A Y": 6 + 2,
+  "A Z": 0 + 3,
+  "B X": 0 + 1,
+  "B Y": 3 + 2,
+  "B Z": 6 + 3,
+  "C X": 6 + 1,
+  "C Y": 0 + 2,
+  "C Z": 3 + 3,
+};
+
+const conversion: { [key: string]: string } = {
+  "A X": "A Z",
+  "A Y": "A X",
+  "A Z": "A Y",
+  "B X": "B X",
+  "B Y": "B Y",
+  "B Z": "B Z",
+  "C X": "C Y",
+  "C Y": "C Z",
+  "C Z": "C X",
 };
 
 export const part1 = (input: string) => {
   const lines = input.split("\n");
 
-  // A rock
-  // B paper
-  // C scissors
-
-  // X rock
-  // Y paper
-  // Z scissors
-
-  const allPoints = [];
-  for (const round of lines) {
-    const [other, me] = round.split(" ");
-
-    allPoints.push(calculatePoints(other, me));
-  }
-
-  return sum(allPoints);
+  return sum(lines.map((round) => points[round]));
 };
 
 export const part2 = (input: string) => {
   const lines = input.split("\n");
 
-  // A rock
-  // B paper
-  // C scissors
-
-  // X I lose
-  // Y draw
-  // Z I win
-
-  // X rock
-  // Y paper
-  // Z scissors
-
-  const allPoints = [];
-  for (const round of lines) {
-    const [other, result] = round.split(" ");
-
-    let me = "";
-    if (other === "A") {
-      if (result === "X") {
-        me = "Z";
-      }
-
-      if (result === "Y") {
-        me = "X";
-      }
-
-      if (result === "Z") {
-        me = "Y";
-      }
-    }
-
-    if (other === "B") {
-      if (result === "X") {
-        me = "X";
-      }
-
-      if (result === "Y") {
-        me = "Y";
-      }
-
-      if (result === "Z") {
-        me = "Z";
-      }
-    }
-
-    if (other === "C") {
-      if (result === "X") {
-        me = "Y";
-      }
-
-      if (result === "Y") {
-        me = "Z";
-      }
-
-      if (result === "Z") {
-        me = "X";
-      }
-    }
-
-    allPoints.push(calculatePoints(other, me));
-  }
-  return sum(allPoints);
+  return sum(lines.map((round) => points[conversion[round]]));
 };
 
 export const main = () => {
