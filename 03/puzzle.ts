2f9739a5e40d5a8c4e775a63c5d3041f38838db9
Luis Carlin <luiscarlin@gmail.com>
2022-12-03 00:31:57 -0500

    non optimized version

--- 03/puzzle.ts
+++ 03/puzzle.ts
@@ -1,17 +1,59 @@
+import { lodash } from "/deps.ts";
+
+const isLowerCase = (letter: string) => {
+  if (
+    letter.toUpperCase() === letter &&
+    letter !== letter.toLowerCase()
+  ) {
+    return false;
+  } else {
+    // 👉️ this runs
+    return true;
+  }
+};
+
 export const part1 = (input: string) => {
   const lines = input.split("\n");
 
   // start coding here
 
-  return lines.length;
+  const repeated = [];
+
+  for (const line of lines) {
+    const first = line.slice(0, line.length / 2).split("");
+    const second = line.slice(line.length / 2).split("");
+
+    repeated.push(...lodash.intersection(first, second));
+  }
+
+  // a: 97
+  //65  A
+
+  return lodash.sum(
+    repeated.map((char) =>
+      isLowerCase(char) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38
+    ),
+  );
 };
 
 export const part2 = (input: string) => {
   const lines = input.split("\n");
 
-  // start coding here
+  const chunks = lodash.chunk(lines, 3, null);
 
-  return lines.length;
+  const repeated = chunks.flatMap((chunk) =>
+    lodash.intersection(
+      chunk[0].split(""),
+      chunk[1].split(""),
+      chunk[2].split(""),
+    )
+  );
+
+  return lodash.sum(
+    repeated.map((char) =>
+      isLowerCase(char) ? char.charCodeAt(0) - 96 : char.charCodeAt(0) - 38
+    ),
+  );
 };
 
 export const main = () => {
