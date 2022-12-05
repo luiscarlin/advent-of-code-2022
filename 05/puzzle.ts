f8a22465e5c5d850ee5bc093adf8e4200814631e
Luis Carlin <luiscarlin@gmail.com>
2022-12-05 06:31:09 -0500

    got the answers, but ugly

--- 05/puzzle.ts
+++ 05/puzzle.ts
@@ -1,17 +1,70 @@
+import { asserts } from "../deps.ts";
+
 export const part1 = (input: string) => {
-  const lines = input.split("\n");
+  const [crates, moves] = input.split("\n\n").map((x) => x.split("\n"));
 
-  // start coding here
+  const stacks: { [key: number]: string[] } = {};
 
-  return lines.length;
+  crates.slice(0, -1).forEach((line) => {
+    const replaced = line.replaceAll("    ", "[ ]");
+
+    const row = replaced.match(/\[( |[A-Z])\]/g)?.map((match) => match[1]);
+
+    row?.forEach((letter, i) => {
+      if (letter !== " ") {
+        stacks[i + 1]
+          ? stacks[i + 1].unshift(letter)
+          : stacks[i + 1] = [letter];
+      }
+    });
+  });
+
+  const instructions = moves.map((line) => line.match(/\d+/g));
+
+  instructions.forEach(([quantity, from, to]) => {
+    for (let i = 0; i < quantity; i++) {
+      const removed = stacks[from].pop();
+
+      asserts.assertExists(removed);
+      stacks[to].push(removed);
+    }
+  });
+
+  return Object.values(stacks).map((s) => s.at(-1)).join("");
 };
 
 export const part2 = (input: string) => {
-  const lines = input.split("\n");
+  const [crates, moves] = input.split("\n\n").map((x) => x.split("\n"));
 
-  // start coding here
+  const stacks: { [key: number]: string[] } = {};
 
-  return lines.length;
+  crates.slice(0, -1).forEach((line) => {
+    const replaced = line.replaceAll("    ", "[ ]");
+
+    const row = replaced.match(/\[( |[A-Z])\]/g)?.map((match) => match[1]);
+
+    row?.forEach((letter, i) => {
+      if (letter !== " ") {
+        stacks[i + 1]
+          ? stacks[i + 1].unshift(letter)
+          : stacks[i + 1] = [letter];
+      }
+    });
+  });
+
+  const instructions = moves.map((line) => line.match(/\d+/g));
+
+  instructions.forEach(([quantity, from, to]) => {
+    const [start, end] = [
+      stacks[from].slice(0, -quantity),
+      stacks[from].slice(-quantity),
+    ];
+
+    stacks[from] = start;
+    stacks[to].push(...end);
+  });
+
+  return Object.values(stacks).map((s) => s.at(-1)).join("");
 };
 
 export const main = () => {
