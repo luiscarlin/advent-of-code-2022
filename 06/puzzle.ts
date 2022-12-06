82de59fab3df91c9bd8a165f2c4a882cd433c757
Luis Carlin <luiscarlin@gmail.com>
2022-12-06 06:05:16 -0500

    day 6 done

--- 06/puzzle.ts
+++ 06/puzzle.ts
@@ -1,17 +1,52 @@
+import { lodash } from "/deps.ts";
 export const part1 = (input: string) => {
-  const lines = input.split("\n");
+  const text = input.split("");
 
-  // start coding here
+  let index = 0;
 
-  return lines.length;
+  for (const i of lodash.range(text.length)) {
+    const subList = text.slice(i, 4 + i);
+
+    if (subList.length !== 4) {
+      break;
+    }
+
+    const allUnque = subList.every((c, index) =>
+      ![...subList.slice(0, index), ...subList.slice(index + 1)].includes(c)
+    );
+
+    if (allUnque) {
+      index = i + 4;
+      break;
+    }
+  }
+
+  return index;
 };
 
 export const part2 = (input: string) => {
-  const lines = input.split("\n");
+  const text = input.split("");
 
-  // start coding here
+  let index = 0;
 
-  return lines.length;
+  for (const i of lodash.range(text.length)) {
+    const subList = text.slice(i, 14 + i);
+
+    if (subList.length !== 14) {
+      break;
+    }
+
+    const allUnque = subList.every((c, index) =>
+      ![...subList.slice(0, index), ...subList.slice(index + 1)].includes(c)
+    );
+
+    if (allUnque) {
+      index = i + 14;
+      break;
+    }
+  }
+
+  return index;
 };
 
 export const main = () => {
