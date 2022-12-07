ca63c262509953575268e60769648620409e7e0a
Luis Carlin <luiscarlin@gmail.com>
2022-12-07 08:50:38 -0500

    refactored the bad

--- 07/puzzle.ts
+++ 07/puzzle.ts
@@ -1,101 +1,56 @@
-import { asserts, lodash } from "../deps.ts";
+import { lodash } from "../deps.ts";
 
-export const part1 = (input: string) => {
+const getDirSizes = (input: string) => {
   const lines = input.split("\n");
 
-  // start coding here
+  const sizes: { [key: string]: number } = {};
+  const path = [];
 
-  const tree: { [key: string]: Set<string> } = {};
-
-  const path: string[] = [];
-
-  const getPath = (dir) => dir ? path.join("/") + "/" + dir : path.join("/");
-
-  lines.forEach((line) => {
+  for (const line of lines) {
     const parts = line.split(" ");
 
-    const curDir = path.join("/");
-
-    if (parts[0] == "$") {
+    if (parts[0] === "$") {
       if (parts[1] == "cd") {
         if (parts[2] === "..") {
           path.pop();
         } else {
           path.push(parts[2]);
         }
+      } else if (parts[1] === "ls") {
+        continue;
       }
+    } else if (parts[0] === "dir") {
+      continue;
     } else {
-      asserts.assertExists(curDir);
+      for (let i = 0; i < path.length; i++) {
+        const fullPathDir = path.slice(0, path.length - i).join("/");
+        const fileSize = Number(parts[0]);
 
-      // I'm assuming this is after doing an ls. If this is not the case, this will blow up.
-      if (parts[0] === "dir") {
-        const childPath = getPath(parts[1]);
-
-        tree[curDir]
-          ? tree[curDir].add(childPath)
-          : tree[curDir] = new Set([childPath]);
-      } else {
-        tree[curDir]
-          ? tree[curDir].add(parts[0])
-          : tree[curDir] = new Set([parts[0]]);
+        sizes[fullPathDir]
+          ? sizes[fullPathDir] += fileSize
+          : sizes[fullPathDir] = fileSize;
       }
     }
-  });
-
-  // traverse tree
-
-  // console.log(tree);
-
-  // return;
-
-  const cache = {};
-
-  function getSize(dir: string) {
-    if (cache[dir]) {
-      return cache[dir];
-    }
-
-    let dirSize = 0;
-
-    for (const item of tree[dir]) {
-      const isDir = isNaN(parseInt(item));
-
-      if (isDir) {
-        const childDirSize = getSize(item);
-        dirSize += childDirSize;
-      } else {
-        dirSize += parseInt(item);
-      }
-    }
-
-    cache[dir] = dirSize;
-
-    return dirSize;
   }
 
-  const rootSize = getSize("/");
-
-  const unusedSpace = 70000000 - rootSize;
-
-  const sizeNeededToRun = 30000000 - unusedSpace;
-
-  // console.log(sizeNeededToRun);
-
-  console.log(
-    lodash.min(Object.values(cache).filter((dir) => dir > sizeNeededToRun)),
-  );
+  return sizes;
+};
+export const part1 = (input: string) => {
+  const sizes = getDirSizes(input);
 
   return lodash.sum(
-    Object.values(cache).filter((folder) => folder <= 100000),
+    Object.values(sizes).filter((folder) => folder <= 100000),
   );
 };
 
 export const part2 = (input: string) => {
-  const lines = input.split("\n");
+  const sizes = getDirSizes(input);
 
-  // start coding here
+  const sizeNeededToRun = 30000000 - (70000000 - sizes["/"]);
 
-  return lines.length;
+  return lodash.min(
+    Object.values(sizes).filter((dir) => dir > sizeNeededToRun),
+  );
 };
 
 export const main = () => {
