adb2fb0c8bbfec5ed7201306f2157729a6757f8e
Luis Carlin <luiscarlin@gmail.com>
2022-12-08 13:00:49 -0500

    clean up a bit

--- 08/puzzle.ts
+++ 08/puzzle.ts
@@ -1,6 +1,6 @@
-import { lodash } from "/deps.ts";
+import { asserts, lodash } from "/deps.ts";
 export const part1 = (input: string) => {
-  const grid = input.split("\n").map((r) => r.split("").map(Number));
+  const grid = input.split("\n").map(([...line]) => line.map(Number));
 
   let numVisible = 2 * grid.length + 2 * (grid[0].length - 2);
 
@@ -14,7 +14,7 @@ export const part1 = (input: string) => {
         continue;
       }
 
-      const allTreesToEdge: number[][] = [
+      const atLeastVisibleFromOneSide = [
         lodash.range(0, col).map((coli: number) => grid[row][coli]),
         lodash.range(0, row).map((rowi: number) => grid[rowi][col]),
         lodash.range(row + 1, grid.length).map((rowi: number) =>
@@ -23,11 +23,9 @@ export const part1 = (input: string) => {
         lodash.range(col + 1, grid[row].length).map((coli: number) =>
           grid[row][coli]
         ),
-      ];
-
-      const atLeastVisibleFromOneSide = allTreesToEdge.map((treesInLine) =>
-        treesInLine.every((tree) => tree < grid[row][col])
-      ).some((visible) => visible);
+      ].some((side) =>
+        side.every((treeOnSide: number) => treeOnSide < grid[row][col])
+      );
 
       if (atLeastVisibleFromOneSide) {
         numVisible++;
@@ -35,11 +33,13 @@ export const part1 = (input: string) => {
     }
   }
 
+  asserts.assertEquals(numVisible, 1851);
+
   return numVisible;
 };
 
 export const part2 = (input: string) => {
-  const grid = input.split("\n").map((r) => r.split("").map(Number));
+  const grid = input.split("\n").map(([...line]) => line.map(Number));
 
   let highestScenicScore = 0;
 
@@ -72,11 +72,11 @@ export const part2 = (input: string) => {
 
       const scenicScore = numVisibleTrees.reduce((acc, num) => acc *= num, 1);
 
-      if (scenicScore > highestScenicScore) {
-        highestScenicScore = scenicScore;
-      }
+      highestScenicScore = Math.max(scenicScore, highestScenicScore);
     }
   }
+
+  asserts.assertEquals(highestScenicScore, 574080);
 
   return highestScenicScore;
 };
