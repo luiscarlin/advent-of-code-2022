743b79b4819a4d4d08f69f9eeb2f77538803ebc5
Luis Carlin <luiscarlin@gmail.com>
2022-12-08 08:38:34 -0500

    part 1 and 2 complete. needs cleanup

--- 08/puzzle.ts
+++ 08/puzzle.ts
@@ -1,17 +1,90 @@
+import { lodash } from "/deps.ts";
 export const part1 = (input: string) => {
-  const lines = input.split("\n");
+  const grid = input.split("\n").map((r) => r.split("").map(Number));
 
-  // start coding here
+  let numVisible = 2 * grid.length + 2 * (grid[0].length - 2);
 
-  return lines.length;
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      // skip edges
+      if (
+        row == 0 || row === grid.length - 1 || col === 0 ||
+        col === grid[0].length - 1
+      ) {
+        continue;
+      }
+
+      const allTreesToEdge: number[][] = [
+        lodash.range(0, col).map((coli: number) => grid[row][coli]),
+        lodash.range(0, row).map((rowi: number) => grid[rowi][col]),
+        lodash.range(row + 1, grid.length).map((rowi: number) =>
+          grid[rowi][col]
+        ),
+        lodash.range(col + 1, grid[row].length).map((coli: number) =>
+          grid[row][coli]
+        ),
+      ];
+
+      const atLeastVisibleFromOneSide = allTreesToEdge.map((treesInLine) =>
+        treesInLine.every((tree) => tree < grid[row][col])
+      ).some((visible) => visible);
+
+      if (atLeastVisibleFromOneSide) {
+        numVisible++;
+      }
+    }
+  }
+
+  return numVisible;
 };
 
 export const part2 = (input: string) => {
-  const lines = input.split("\n");
+  const grid = input.split("\n").map((r) => r.split("").map(Number));
 
-  // start coding here
+  let numVisible = 2 * grid.length + 2 * (grid[0].length - 2);
 
-  return lines.length;
+  let highestScenicScore = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      // skip edges
+      if (
+        row == 0 || row === grid.length - 1 || col === 0 ||
+        col === grid[0].length - 1
+      ) {
+        continue;
+      }
+
+      const allTreesToEdge: number[][] = [
+        lodash.range(0, col).map((coli: number) => grid[row][coli]).reverse(),
+        lodash.range(0, row).map((rowi: number) => grid[rowi][col]).reverse(),
+        lodash.range(row + 1, grid.length).map((rowi: number) =>
+          grid[rowi][col]
+        ),
+        lodash.range(col + 1, grid[row].length).map((coli: number) =>
+          grid[row][coli]
+        ),
+      ];
+
+      const numVisibleTrees = allTreesToEdge.map((treesInLine) => {
+        const index = treesInLine.findIndex((tree) => tree >= grid[row][col]);
+
+        if (index === -1) {
+          return treesInLine.length;
+        }
+
+        return index + 1;
+      });
+
+      const scenicScore = numVisibleTrees.reduce((acc, num) => acc *= num, 1);
+
+      if (scenicScore > highestScenicScore) {
+        highestScenicScore = scenicScore;
+      }
+    }
+  }
+
+  return highestScenicScore;
 };
 
 export const main = () => {
