a3b2c64be6572799928a79b2c63279f66442075a
Luis Carlin <luiscarlin@gmail.com>
2022-12-12 07:53:11 -0500

    part 2 done

--- 10/puzzle.ts
+++ 10/puzzle.ts
@@ -1,3 +1,7 @@
+const enc = (s: string) => new TextEncoder().encode(s);
+
+const write = async (s: string) => await Deno.stdout.write(enc(s));
+
 export const part1 = (input: string) => {
   const programStack = input.split("\n").reverse();
 
@@ -48,15 +52,73 @@ export const part1 = (input: string) => {
   return signalSum;
 };
 
-export const part2 = (input: string) => {
-  const lines = input.split("\n");
+export const part2 = async (input: string) => {
+  const programStack = input.split("\n").reverse();
 
-  // start coding here
+  let registerX = 1;
+  let cycleCounter = 1;
+  let pendingInstruction: string[] | undefined = [];
+  let signalSum = 0;
+  let isProgramDone = false;
 
-  return lines.length;
+  const printCRT = async () => {
+    const pixelPosition = (cycleCounter - 1) % 40;
+    let char = ".";
+
+    if ([registerX - 1, registerX, registerX + 1].includes(pixelPosition)) {
+      char = "#";
+    }
+
+    if (pixelPosition === 39) {
+      char += "\n";
+    }
+
+    await write(char);
+  };
+
+  while (!isProgramDone) {
+    // debug
+    // console.log(
+    //   `=== cycle: ${cycleCounter}, regX: ${registerX}, signal: ${
+    //     registerX * cycleCounter
+    //   } ==`,
+    // );
+
+    if (cycleCounter === 20 || (cycleCounter - 20) % 40 === 0) {
+      signalSum += cycleCounter * registerX;
+    }
+    await printCRT();
+
+    // if pending instruction
+    if (pendingInstruction && pendingInstruction.length > 0) {
+      // pull instruction with args
+      const [inst, args] = [...pendingInstruction];
+      pendingInstruction = [];
+
+      if (inst === "addx") {
+        registerX += Number(args);
+      }
+    } else {
+      // there's no pending instruction
+
+      // get next thing from the program
+      pendingInstruction = programStack.pop()?.split(" ");
+
+      if (!pendingInstruction) {
+        isProgramDone = true;
+      } else if (pendingInstruction[0] === "noop") {
+        pendingInstruction = [];
+      }
+    }
+
+    // tick
+    cycleCounter += 1;
+  }
+
+  return signalSum;
 };
 
-export const main = () => {
+export const main = async () => {
   const [file] = Deno.args;
 
   const input = Deno.readTextFileSync(
@@ -64,7 +126,7 @@ export const main = () => {
   );
 
   console.log("part1", part1(input));
-  console.log("part2", part2(input));
+  console.log("part2", await part2(input));
 };
 
-import.meta.main && main();
+import.meta.main && await main();
