a9d52734109f498bb80849e809fff0b2440de0c5
Luis Carlin <luiscarlin@gmail.com>
2022-12-12 06:43:31 -0500

    part 1 done

--- 10/puzzle.ts
+++ 10/puzzle.ts
@@ -1,9 +1,51 @@
 export const part1 = (input: string) => {
-  const lines = input.split("\n");
+  const programStack = input.split("\n").reverse();
 
-  // start coding here
+  let registerX = 1;
+  let cycleCounter = 1;
+  let pendingInstruction: string[] | undefined = [];
+  let signalSum = 0;
+  let isProgramDone = false;
 
-  return lines.length;
+  while (!isProgramDone) {
+    // debug
+    // console.log(
+    //   `=== cycle: ${cycleCounter}, regX: ${registerX}, signal: ${
+    //     registerX * cycleCounter
+    //   } ==`,
+    // );
+
+    if (cycleCounter === 20 || (cycleCounter - 20) % 40 === 0) {
+      signalSum += cycleCounter * registerX;
+    }
+
+    // if pending instruction
+    if (pendingInstruction && pendingInstruction.length > 0) {
+      // pull instruction with args
+      const [inst, args] = [...pendingInstruction];
+      pendingInstruction = [];
+
+      if (inst === "addx") {
+        registerX += Number(args);
+      }
+    } else {
+      // there's no pending instruction
+
+      // get next thing from the program
+      pendingInstruction = programStack.pop()?.split(" ");
+
+      if (!pendingInstruction) {
+        isProgramDone = true;
+      } else if (pendingInstruction[0] === "noop") {
+        pendingInstruction = [];
+      }
+    }
+
+    // tick
+    cycleCounter += 1;
+  }
+
+  return signalSum;
 };
 
 export const part2 = (input: string) => {
