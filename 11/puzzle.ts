0996a6696a89ab1e634471225882c29da28b8f4b
Luis Carlin <luiscarlin@gmail.com>
2022-12-13 11:35:37 -0500

    fix part 2

--- 11/puzzle.ts
+++ 11/puzzle.ts
@@ -8,57 +8,70 @@ interface Monkey {
 }
 
 export const part1 = () => {
+  const monkeys: Monkey[] = [{
+    items: [73, 77],
+    operation: (old) => old * 5,
+    div: 11,
+    test: (num, div) => num % div === 0 ? 6 : 5,
+  }, {
+    items: [57, 88, 80],
+    operation: (old) => old + 5,
+    div: 19,
+    test: (num, div) => num % div === 0 ? 6 : 0,
+  }, {
+    items: [61, 81, 84, 69, 77, 88],
+    operation: (old) => old * 19,
+    div: 5,
+    test: (num, div) => num % div === 0 ? 3 : 1,
+  }, {
+    items: [78, 89, 71, 60, 81, 84, 87, 75],
+    operation: (old) => old + 7,
+    div: 3,
+    test: (num, div) => num % div === 0 ? 1 : 0,
+  }, {
+    items: [60, 76, 90, 63, 86, 87, 89],
+    operation: (old) => old + 2,
+    div: 13,
+    test: (num, div) => num % div === 0 ? 2 : 7,
+  }, {
+    items: [88],
+    operation: (old) => old + 1,
+    div: 17,
+    test: (num, div) => num % div === 0 ? 4 : 7,
+  }, {
+    items: [84, 98, 78, 85],
+    operation: (old) => old * old,
+    div: 7,
+    test: (num, div) => num % div === 0 ? 5 : 4,
+  }, {
+    items: [98, 89, 78, 73, 71],
+    operation: (old) => old + 4,
+    div: 2,
+    test: (num, div) => num % div === 0 ? 3 : 2,
+  }];
+
   // const monkeys: Monkey[] = [{
-  //   items: [73, 77],
-  //   operation: (old) => old * 5,
-  //   test: (num) => num % 11 === 0 ? 6 : 5,
-  // }, {
-  //   items: [57, 88, 80],
-  //   operation: (old) => old + 5,
-  //   test: (num) => num % 19 === 0 ? 6 : 0,
-  // }, {
-  //   items: [61, 81, 84, 69, 77, 88],
+  //   items: [79, 98],
   //   operation: (old) => old * 19,
-  //   test: (num) => num % 5 === 0 ? 3 : 1,
+  //   div: 23,
+  //   test: (num, div) => num % div === 0 ? 2 : 3,
   // }, {
-  //   items: [78, 89, 71, 60, 81, 84, 87, 75],
-  //   operation: (old) => old + 7,
-  //   test: (num) => num % 3 === 0 ? 1 : 0,
+  //   items: [54, 65, 75, 74],
+  //   operation: (old) => old + 6,
+  //   div: 19,
+  //   test: (num, div) => num % div === 0 ? 2 : 0,
   // }, {
-  //   items: [60, 76, 90, 63, 86, 87, 89],
-  //   operation: (old) => old + 2,
-  //   test: (num) => num % 13 === 0 ? 2 : 7,
-  // }, {
-  //   items: [88],
-  //   operation: (old) => old + 1,
-  //   test: (num) => num % 17 === 0 ? 4 : 7,
-  // }, {
-  //   items: [84, 98, 78, 85],
+  //   items: [79, 60, 97],
   //   operation: (old) => old * old,
-  //   test: (num) => num % 7 === 0 ? 5 : 4,
+  //   div: 13,
+  //   test: (num, div) => num % div === 0 ? 1 : 3,
   // }, {
-  //   items: [98, 89, 78, 73, 71],
-  //   operation: (old) => old + 4,
-  //   test: (num) => num % 2 === 0 ? 3 : 2,
+  //   items: [74],
+  //   operation: (old) => old + 3,
+  //   div: 17,
+  //   test: (num, div) => num % div === 0 ? 0 : 1,
   // }];
 
-  const monkeys: Monkey[] = [{
-    items: [79, 98],
-    operation: (old) => old * 19,
-    test: (num) => num % 23 === 0 ? 2 : 3,
-  }, {
-    items: [54, 65, 75, 74],
-    operation: (old) => old + 6,
-    test: (num) => num % 19 === 0 ? 2 : 0,
-  }, {
-    items: [79, 60, 97],
-    operation: (old) => old * old,
-    test: (num) => num % 13 === 0 ? 1 : 3,
-  }, {
-    items: [74],
-    operation: (old) => old + 3,
-    test: (num) => num % 17 === 0 ? 0 : 1,
-  }];
   const monkeyBusiness = Array(8).fill(0);
 
   let round = 0;
@@ -71,10 +84,12 @@ export const part1 = () => {
         const worry = monkeys[i].items.shift();
         monkeyBusiness[i]++;
 
+        asserts.assertExists(worry);
+
         let newWorry = monkeys[i].operation(worry);
         newWorry = Math.floor(newWorry / 3);
 
-        const sendToIndex = monkeys[i].test(newWorry);
+        const sendToIndex = monkeys[i].test(newWorry, monkeys[i].div);
 
         monkeys[sendToIndex].items.push(newWorry);
       }
